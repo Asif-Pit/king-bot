2d712b8c6a9710c3be853330e6f57c9af81978e2
Dark-Asif <101747704+Dark-Asif@users.noreply.github.com>
2022-03-27 19:41:28 +0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,10 +4,12 @@ function handler(m) {
   this.sendContact(m.chat, '923474187615', '𝗛𝗲𝗮𝗿𝘁 𝗛𝗮𝗰𝗸𝗲𝗿❤️ ', m)
   // F this.sendContact(m.chat, '27671805497', 'DENVIN💙', m)
   this.sendContact(m.chat, '27671805497', 'DENVIN💙 ', m)
-  // F this.sendContact(m.chat, '201140012751', 'ADONIS💗, m)
+  // F this.sendContact(m.chat, '201140012751', 'ADONIS💗', m)
   this.sendContact(m.chat, '201140012751', 'ADONIS💗 ', m)
   // F this.sendContact(m.chat, '265l994198040', 'TREM😹', m)
   this.sendContact(m.chat, '265994198040', 'TREM😹 ', m)
+  // F this.sendContact(m.chat, '27634090203', 'DRIPS ',m)
+  this.sendContact(m.chat, '27634090203', 'DRIPS♥️ ', m)
 }
 handler.help = ['owner/creator']
 handler.tags = ['info']
